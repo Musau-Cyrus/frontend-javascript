a352455c04f744396591d4677f128eb6c1305a5f
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-05-25 21:43:26 +0300

    Namespace and Declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,17 @@
+namespace Subjects{
+    export class Teacher{
+        experienceTeachingReact: number;
+    }
+    class React extends Subject{
+          getRequirements(): string{
+            return 'Here is a requirements for React';
+        }
+
+        getAvailableTeacher(): string{
+            if (this.teacher && this.teacher.experienceTeachingReact > 0) {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            }
+            return "No available teacher";
+        }
+    }
+}
